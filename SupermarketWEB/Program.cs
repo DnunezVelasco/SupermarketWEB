5bb5c7281026a61265819018eb36df68440a2f1a
DAVID ALEJANDRO NUNEZ VELASCO <dnunez1482@campuscecep.edu.co>
2023-05-06 21:19:04 -0500

    Modelos y configuración del DbContext

--- SupermarketWEB/Program.cs
+++ SupermarketWEB/Program.cs
@@ -1,31 +1,52 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SupermarketWEB.Data;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-
-
-//add services to the
-builder.Services.AddRazorPages();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
- 
+internal class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+
+        builder.Services.AddDbContext<SupermarketContext>(Options =>
+      Options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB")));
+
+        var app = builder.Build();
+
+
+
+
+
+
+
+        //add services to the
+        builder.Services.AddRazorPages();
+
+
+
+
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
