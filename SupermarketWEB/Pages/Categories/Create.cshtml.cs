c8d8e58ddf12394382d2623c56d7036f686a539e
DAVID ALEJANDRO NUNEZ VELASCO <dnunez1482@campuscecep.edu.co>
2023-05-13 20:24:10 -0500

    Categori Create 2.0

--- SupermarketWEB/Pages/Categories/Create.cshtml.cs
+++ SupermarketWEB/Pages/Categories/Create.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Categories
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+
+        public Category Category { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Categories == null || Category == null)
+            {
+                return Page();
+            }
+
+            _context.Categories.Add(Category);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
         }
     }
 }
